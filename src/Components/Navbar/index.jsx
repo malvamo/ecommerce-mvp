91a3620e2e7f65113460aa878ae24b1e7210c4bc
Manuel Valbuena <valbuenamoralesmanuelalejandro@gmail.com>
2023-09-28 16:23:52 -0500

    Componente Navbar

---
+++ src/Components/Navbar/index.jsx
@@ -0,0 +1,96 @@
+import { NavLink } from "react-router-dom";
+
+
+
+const Navbar = () => {
+
+    const activeStyle = "underline underline-offset-4"
+
+    return (
+        <nav className="flex justify-between items-center fixed z-10 w-full py-5 px-8 text-sm font-light">
+            <ul className="flex items-center gap-3">
+                <li className="font-semibold text-lg">
+                    <NavLink 
+                        to='/'>
+                        Shopi
+                    </NavLink>
+                </li>
+                <li>
+                    <NavLink 
+                        to='/'
+                        className={({ isActive }) => isActive ? activeStyle : undefined 
+                        }>
+                        All
+                    </NavLink>
+                </li>
+                <li>
+                    <NavLink 
+                        to='/clothes'
+                        className={({ isActive }) => isActive ? activeStyle : undefined 
+                        }>
+                        Clothes
+                    </NavLink>
+                </li>
+                <li>
+                    <NavLink 
+                        to='/electronics'
+                        className={({ isActive }) => isActive ? activeStyle : undefined 
+                        }>
+                        Electronics
+                    </NavLink>
+                </li>
+                <li>
+                    <NavLink 
+                        to='/furnitures'
+                        className={({ isActive }) => isActive ? activeStyle : undefined 
+                        }>
+                        Furnitures
+                    </NavLink>
+                </li><li>
+                    <NavLink 
+                        to='/toys'
+                        className={({ isActive }) => isActive ? activeStyle : undefined 
+                        }>
+                        Toys
+                    </NavLink>
+                </li>
+                <li>
+                    <NavLink 
+                        to='/others'
+                        className={({ isActive }) => isActive ? activeStyle : undefined 
+                        }>
+                        Others
+                    </NavLink>
+                </li>
+            </ul>
+            <ul className="flex items-center gap-3">
+                <li className="text-black/60">
+                    mvp@gmail.com
+                </li>
+                <li>
+                    <NavLink to='/'>
+                        My Orders
+                    </NavLink>
+                </li>
+                <li>
+                    <NavLink to='/clothes'>
+                        My Account
+                    </NavLink>
+                </li>
+                <li>
+                    <NavLink to='/electronics'>
+                        Sign In
+                    </NavLink>
+                </li>
+                <li>
+                    <NavLink to='/furnitures'>
+                        Carrito 0
+                    </NavLink>
+                </li>
+            </ul>
+
+        </nav>
+    );
+}
+
+export default Navbar
