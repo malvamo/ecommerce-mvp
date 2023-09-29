e8322ff23eafe1e4f7186fed1f70a4ac0fb81297
Manuel Valbuena <valbuenamoralesmanuelalejandro@gmail.com>
2023-09-29 14:15:25 -0500

    SideMenu del carrito de compras

--- src/Context/index.jsx
+++ src/Context/index.jsx
@@ -3,15 +3,23 @@ import { useState, createContext } from 'react';
 export const ShopingCartContext = createContext();
 
 export const ShopingCartProvider =  ({ children }) => {
-
+    // Shopping Cart | Increment quantity
     const [count, setCount] = useState(0)
 
+    // Product Detail | Open/Close
     const [isProductDetailOpen, setIsProductDetailOpen] = useState(false)
     const openProductDetail = () => setIsProductDetailOpen(true)
     const closeProductDetail = () => setIsProductDetailOpen(false)
 
+    // Checkout Side Menu | Open/Close
+    const [isCheckoutSideMenuOpen, setIsCheckoutSideMenuOpen] = useState(false)
+    const openCheckoutSideMenu = () => setIsCheckoutSideMenuOpen(true)
+    const closeCheckoutSideMenu = () => setIsCheckoutSideMenuOpen(false)
+
+    // Product Detail | Show product
     const [productToShow, setProductToShow] = useState({})
 
+    // Shopping Cart | Add products to cart
     const [cartProducts, setCartProducts] = useState([])
 
     return (
@@ -25,6 +33,9 @@ export const ShopingCartProvider =  ({ children }) => {
             setProductToShow,
             cartProducts,
             setCartProducts,
+            isCheckoutSideMenuOpen,
+            openCheckoutSideMenu,
+            closeCheckoutSideMenu,
         }}>
             {children}
         </ShopingCartContext.Provider>
