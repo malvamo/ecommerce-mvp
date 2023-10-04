32a43fc7e487915b7d428b1ed05fe60c1b68be01
Manuel Valbuena <valbuenamoralesmanuelalejandro@gmail.com>
2023-10-04 09:34:39 -0500

    Flujo para crear una nueva orden

--- src/Components/CheckoutSideMenu/index.jsx
+++ src/Components/CheckoutSideMenu/index.jsx
@@ -14,6 +14,18 @@ const CheckoutSideMenu = () => {
         context.setCartProducts(filteredProducts)
     }
 
+    const handleCheckout = () => {
+        const orderToAdd = {
+            date: '01.02.23',
+            products: context.cartProducts,
+            totalProducts: context.cartProducts.length,
+            totalPrice: totalPrice(context.cartProducts)
+        }
+
+        context.setOrder([...context.order, orderToAdd])
+        context.setCartProducts([])
+    }
+
     return (
         <aside
             className={`${context.isCheckoutSideMenuOpen ? 'flex' : 'hidden'} checkout-side-menu flex-col fixed right-0 border border-black rounded-lg bg-white overflow-auto`}>
@@ -23,7 +35,7 @@ const CheckoutSideMenu = () => {
                     className='h-6 w-6 text-black cursor-pointer'
                     onClick={() => context.closeCheckoutSideMenu()} /></div>
             </div>
-            <div className='px-6 overflow-y-scroll'>
+            <div className='px-6 overflow-y-scroll flex-1'>
                 {
                     context.cartProducts.map(product => (
                         <OrderCard
@@ -37,11 +49,12 @@ const CheckoutSideMenu = () => {
                     ))
                 }
             </div>
-            <div className='px-6'>
-                <p className='flex justify-between items-center'>
+            <div className='px-6 mb-6'>
+                <p className='flex justify-between items-center mb-2'>
                     <span className='font-light'>Total:</span>
                     <span className='font-medium text-2xl'>${totalPrice(context.cartProducts)}</span>
                 </p>
+                <button className='bg-black py-3 text-white w-full rounded-lg' onClick={() => handleCheckout()}></button>
             </div>
         </aside>
     );
