781b9d9bbcd6e1d1c0c11add92de3a0d73d02939
Manuel Valbuena <valbuenamoralesmanuelalejandro@gmail.com>
2023-10-04 10:46:05 -0500

    Página de MyOrders: lista de órdenes

--- src/Pages/MyOrders/index.jsx
+++ src/Pages/MyOrders/index.jsx
@@ -1,9 +1,28 @@
+import { useContext } from 'react';
+import { Link } from 'react-router-dom';
 import Layout from '../../Components/Layout';
+import { ShopingCartContext } from '../../Context';
+import OrdersCard from '../../Components/OrdersCard';
 
 function MyOrders() {
+
+    const context = useContext(ShopingCartContext)
+
     return (
         <Layout>
-            MyOrders
+            <div className='flex items-center justify-center relative w-80'>
+                <h1>My Orders</h1>
+            </div>
+            {
+                context.order.map((order, index) => {
+                    <Link key={index} to={`/my-orders/${order.id}`}>
+                        <OrdersCard
+                            totalPrice={order.totalPrice}
+                            totalProducts={order.totalProducts}
+                            />
+                    </Link>
+                })
+            }
         </Layout>
     );
 }
