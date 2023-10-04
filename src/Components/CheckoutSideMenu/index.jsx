0dc5a28b0e0b63e37ef4bf7ab723154f3596f62d
Manuel Valbuena <valbuenamoralesmanuelalejandro@gmail.com>
2023-10-04 10:02:34 -0500

    Checkout de productos en el carrito

--- src/Components/CheckoutSideMenu/index.jsx
+++ src/Components/CheckoutSideMenu/index.jsx
@@ -1,4 +1,5 @@
 import { useContext } from 'react'
+import { Link } from 'react-router-dom'
 import { ShopingCartContext } from '../../Context'
 import { XMarkIcon } from '@heroicons/react/24/solid'
 import OrderCard from '../OrderCard'
@@ -54,7 +55,9 @@ const CheckoutSideMenu = () => {
                     <span className='font-light'>Total:</span>
                     <span className='font-medium text-2xl'>${totalPrice(context.cartProducts)}</span>
                 </p>
-                <button className='bg-black py-3 text-white w-full rounded-lg' onClick={() => handleCheckout()}></button>
+                <Link to='/my-orders/last'>
+                    <button className='bg-black py-3 text-white w-full rounded-lg' onClick={() => handleCheckout()}></button>
+                </Link>
             </div>
         </aside>
     );
--- src/Pages/MyOrder/index.jsx
+++ src/Pages/MyOrder/index.jsx
@@ -1,9 +1,28 @@
+import { useContext } from 'react';
+import { ShopingCartContext } from '../../Context'
 import Layout from '../../Components/Layout';
+import OrderCard from '../../Components/OrderCard';
 
 function MyOrder() {
+
+    const context = useContext(ShopingCartContext)
+
     return (
         <Layout>
             MyOrder
+            <div className='flex flex-col w-80'>
+                {
+                    context.order?.slice(-1)[0].products.map(product => (
+                        <OrderCard
+                            key={product.id}
+                            id={product.id}
+                            title={product.title}
+                            imageUrl={product.image}
+                            price={product.price}
+                        />
+                    ))
+                }
+            </div>
         </Layout>
     );
 }
